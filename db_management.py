8346689785090d2fb2d53f72d5594bfdea016453
Malcolm <malcolm@shredsauce.com>
2024-03-26 15:02:52 -0400

    Try-catch for dropping database to prevent error if the database does not yet exist. Clean up: Functions for connection, dropping/creating db, creating tables and populating db.

--- db_management.py
+++ db_management.py
@@ -1,10 +1,14 @@
-if __name__ == '__main__':
-    import mysql.connector
-    from mysql.connector import Error
-    import configparser
+import mysql.connector
+from mysql.connector import Error
+import configparser
 
+config_path = 'config.ini'
+db_name = 'soen_project_phase_1'
+
+
+def main():
     config = configparser.ConfigParser()
-    config.read('config.ini')
+    config.read(config_path)
 
     db_config = {
         'user': config.get('database', 'user'),
@@ -13,51 +17,80 @@ if __name__ == '__main__':
         'raise_on_warnings': config.getboolean('database', 'raise_on_warnings')
     }
 
-    db_name = 'soen_project_phase_1'
-
     try:
-        connection = mysql.connector.connect(**db_config)
-        cursor = connection.cursor()
-        cursor.execute(f"DROP DATABASE IF EXISTS {db_name};")
-        cursor.execute(f"CREATE DATABASE {db_name};")
-        cursor.close()
-        connection.close()
+        connection = create_connection(db_config)
+        drop_existing_db(connection)
+        close_connection(connection)
 
-        connection_config_dict = db_config.copy()
-        connection_config_dict['database'] = db_name
-        connection = mysql.connector.connect(**connection_config_dict)
-        cursor = connection.cursor()
-
-        ddl_sql_query = """
-        USE soen_project_phase_1;
-    
-        CREATE TABLE game (
-            game_id INT AUTO_INCREMENT PRIMARY KEY,
-            name VARCHAR(255),
-            igdb_key VARCHAR(255)
-        );
-        """
-
-        for result in cursor.execute(ddl_sql_query, multi=True):
-            if result.with_rows:
-                print(result.fetchall())
-
-        dml_sql_query = """
-        INSERT INTO game (name, igdb_key) VALUES ('some name', 'some_ig_somekey');
-        """
-
-        for result in cursor.execute(dml_sql_query, multi=True):
-            if result.with_rows:
-                print(result.fetchall())
-
-        connection.commit()
+        connection = create_connection(db_config)
+        create_database(connection)
+        create_tables(connection)
+        populate_database(connection)
+        close_connection(connection)
 
         print("Statements executed successfully.")
-
     except Error as e:
         print("Error while connecting to MySQL", e)
-    finally:
-        if connection.is_connected():
-            cursor.close()
-            connection.close()
-            print("MySQL connection is closed")
+
+
+def drop_existing_db(connection):
+    cursor = connection.cursor()
+
+    try:
+        cursor.execute(f"DROP DATABASE IF EXISTS {db_name};")
+    except mysql.connector.Error as err:
+        if err.errno == 1008:
+            pass
+        else:
+            raise
+
+
+def create_database(connection):
+    cursor = connection.cursor()
+    cursor.execute(f"CREATE DATABASE {db_name};")
+
+
+def create_tables(connection):
+    cursor = connection.cursor()
+
+    sql_query = f"""
+    USE {db_name};
+
+    CREATE TABLE game (
+        game_id INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(255),
+        igdb_key VARCHAR(255)
+    );
+    """
+
+    for result in cursor.execute(sql_query, multi=True):
+        if result.with_rows:
+            print(result.fetchall())
+
+
+def populate_database(connection):
+    cursor = connection.cursor()
+
+    dml_sql_query = """
+    INSERT INTO game (name, igdb_key) VALUES ('some name', 'some_ig_somekey');
+    """
+
+    for result in cursor.execute(dml_sql_query, multi=True):
+        if result.with_rows:
+            print(result.fetchall())
+
+    connection.commit()
+
+
+def create_connection(db_config):
+    return mysql.connector.connect(**db_config)
+
+
+def close_connection(connection):
+    cursor = connection.cursor()
+    cursor.close()
+    connection.close()
+
+
+if __name__ == "__main__":
+    main()
